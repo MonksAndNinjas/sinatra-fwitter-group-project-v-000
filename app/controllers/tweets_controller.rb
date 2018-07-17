b7143efb0adb1a8a8e75ee3b1d83af8b94566ee3
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-17 05:52:36 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,53 @@
+require './config/environment'
+
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if Helpers.is_logged_in?(session)
+      @current_user = User.find_by_id(session[:user_id])
+
+      erb :'/tweets/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+
+    erb :'/tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+
+    erb :'/tweets/edit'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.new(params)
+    @tweet.save
+
+    redirect to '/tweets'
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.update(content: params[:content])
+    @tweet.save
+
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+
+    redirect to '/tweets'
+  end
 
 end
